0d554b948398024cfbbfc17b168265b8c680f916
Marc <marc.canals.7e4@itb.cat>
2022-06-11 16:42:53 +0200

    Added pushing capabilities

---
+++ Assets/Scripts/Player/PlayerCollisionController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionController : MonoBehaviour
+{
+    private Rigidbody body;
+    private Vector3 force;
+    private float pushPower = 3f, weight = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        body = hit.collider.attachedRigidbody;
+
+        //No RB
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3)
+        {
+            force = new Vector3(0, -0.5f, 0) * (-9.8f) * weight;
+        }
+        else
+        {
+            force = hit.controller.velocity * pushPower;
+        }
+
+        // Apply the push
+        body.AddForceAtPosition(force, hit.point);
+    }
+  
+}
